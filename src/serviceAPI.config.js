3be0c3ebb704aba5e6c413d129d949eebc047e1f
LemonK <932684335@qq.com>
2018-08-12 16:31:17 +0800

    数据接口文件

---
+++ src/serviceAPI.config.js
@@ -0,0 +1,7 @@
+const BASEURL = "https://www.easy-mock.com/mock/5b690a9487dadc0640e32b96/shop/"
+const URL = {
+    getShoppingMallInfo:BASEURL+'index',
+    getGoodsInfo:BASEURL+'getGoodsInfo'
+}
+
+module.exports = URL
