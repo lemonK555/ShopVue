c65673afd0de9e190a2b134fe56aa6d43b757a70
LemonK <932684335@qq.com>
2018-08-13 23:45:49 +0800

    接口文件

--- src/serviceAPI.config.js
+++ src/serviceAPI.config.js
@@ -1,7 +1,9 @@
 const BASEURL = "https://www.easy-mock.com/mock/5b690a9487dadc0640e32b96/shop/"
+const LOCALURL = "http://localhost:3000/"
 const URL = {
     getShoppingMallInfo:BASEURL+'index',
-    getGoodsInfo:BASEURL+'getGoodsInfo'
+    getGoodsInfo:BASEURL+'getGoodsInfo',
+    registerUser:LOCALURL+'user/register',//用户注册接口
 }
 
 module.exports = URL
