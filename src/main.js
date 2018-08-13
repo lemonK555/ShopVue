add5dbd1d30409801f64286ee63681369d889226
LemonK <932684335@qq.com>
2018-08-13 23:46:35 +0800

    增加了vant部分插件

--- src/main.js
+++ src/main.js
@@ -1,13 +1,18 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import { Button,Row,Col,Search,Swipe,SwipeItem,Lazyload,List } from 'vant'
+import { Button,Row,Col,Search,Swipe,SwipeItem,Lazyload,List,Field,NavBar } from 'vant'
+// import VueAwesomeSwiper from 'vue-awesome-swiper'
 
+// import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 Vue.use(Button).use(Row).use(Col).use(Search)
-.use(Swipe).use(SwipeItem).use(Lazyload).use(List);
+.use(Swipe).use(SwipeItem).use(Lazyload).use(List).use(Field).use(NavBar);
 
+// Vue.use(VueAwesomeSwiper);
+// // 设置全局属性
+// Vue.prototype.HOST = '/api'
 new Vue({
   el: '#app',
   router,
