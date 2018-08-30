70e8b304463eece36f60d4cee5b087c79f313064
lemonK555 <932684335@qq.com>
2018-08-30 12:34:02 +0800

    缺个人中心路由

--- src/router/index.js
+++ src/router/index.js
@@ -2,19 +2,48 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import ShoppingMall from '@/components/page/ShoppingMall'
 import Register from '@/components/page/Register'
+import Login from '@/components/page/Login'
+import Goods from '@/components/page/Goods'
+import CategoryList from '@/components/page/CategoryList'
+import Cart from '@/components/page/Cart'
+import Main from '@/components/page/Main'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'ShoppingMall',
-      component: ShoppingMall
-    },{
+      path: '/main',
+      name: 'Main',
+      component: Main,
+      children:[
+        {
+          path: '/',
+          name: 'ShoppingMall',
+          component: ShoppingMall
+        },{
+          path: '/categoryList',
+          name: 'CategoryList',
+          component: CategoryList
+        },{
+          path: '/cart',
+          name: 'Cart',
+          component: Cart
+        },
+      ]
+    },
+    {
       path: '/register',
       name: 'Register',
       component: Register
+    },{
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },{
+      path: '/goods',
+      name: 'Goods',
+      component: Goods
     }
   ]
 })
