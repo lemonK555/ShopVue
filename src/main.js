a5109a9caa0b77a147ed00215b1b2aecb655ec7f
lemonK555 <932684335@qq.com>
2018-08-30 12:35:17 +0800

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import { Button,Row,Col,Search,Swipe,SwipeItem,Lazyload,List,Field,NavBar } from 'vant'
+import { Button,Row,Col,Search,Swipe,SwipeItem,Lazyload,List
+	,Field,NavBar,Tab,Tabs,PullRefresh,Tabbar,TabbarItem} from 'vant'
 // import VueAwesomeSwiper from 'vue-awesome-swiper'
 
 // import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 Vue.use(Button).use(Row).use(Col).use(Search)
-.use(Swipe).use(SwipeItem).use(Lazyload).use(List).use(Field).use(NavBar);
+.use(Swipe).use(SwipeItem).use(Lazyload).use(List)
+.use(Field).use(NavBar).use(Tab).use(Tabs)
+.use(PullRefresh).use(Tabbar).use(TabbarItem);
 
 // Vue.use(VueAwesomeSwiper);
 // // 设置全局属性
