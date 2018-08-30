6fee61a6f31e22f874de7f405270437e548bd04e
lemonK555 <932684335@qq.com>
2018-08-30 12:35:36 +0800

    Add files via upload

--- src/serviceAPI.config.js
+++ src/serviceAPI.config.js
@@ -4,6 +4,11 @@ const URL = {
     getShoppingMallInfo:BASEURL+'index',
     getGoodsInfo:BASEURL+'getGoodsInfo',
     registerUser:LOCALURL+'user/register',//用户注册接口
+    loginUser:LOCALURL+'user/login',//用户登陆接口
+    getDetailGoodsInfo:LOCALURL+'goods/getDetailGoodsInfo',//商品详情接口
+    getCategoryList:LOCALURL+'goods/getCategoryList',//得到大类信息
+    getCategorySubList:LOCALURL+'goods/getCategorySubList',//得到小类信息
+    getGoodsListByCategorySubID:LOCALURL+'goods/getGoodsListByCategorySubID',//得到小类商品信息
 }
 
 module.exports = URL
