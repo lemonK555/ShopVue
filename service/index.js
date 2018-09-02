7380fa4bd680ed8a420058475f062c377310a462
lemonK555 <932684335@qq.com>
2018-09-02 16:58:59 +0800

    服务端相关配置

--- service/index.js
+++ service/index.js
@@ -6,33 +6,30 @@ const bodyParser = require('koa-bodyparser')
 const cors = require('koa2-cors')
 const Router = require('koa-router')
 
+
 app.use(bodyParser());
 app.use(cors());
 
 //引入
 let user = require('./appApi/user.js')
+let goods = require('./appApi/goods.js')
 
 //装载所有子路由
 let router = new Router();
 router.use('/user',user.routes())
+router.use('/goods',goods.routes())
+
 //加载路由中间件
 app.use(router.routes())
 app.use(router.allowedMethods())
-router.post('/', async function (ctx) {
-    ctx.body = '恭喜 __小简__ 你成功登陆了'
-});
-
 
 ;(async()=>{
-	// await connect()
-	// initSchemas()
-    console.log('连接上了吗？')
+	await connect()
+	initSchemas()
 })();
 
 app.use(async(ctx)=>{
-        ctx.body='<h1>404!</h1>';
-    }
-	// ctx.body = '<h1>hello Koa2 嘤嘤嘤</h1>'
+	ctx.body = '<h1>hello Koa2 嘤嘤嘤332</h1>'
 });
 
 app.listen(3000,()=>{
